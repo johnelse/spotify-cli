158e7b9a34f13d97eefe306d2e2d13e8a25d1122
John Else <john.else@gmail.com>
2012-12-07 17:47:56 +0000

    Implement spotify-cli binary.
    Signed-off-by: John Else <john.else@gmail.com>

--- spotify_cli.ml
+++ spotify_cli.ml
@@ -1,2 +1,56 @@
-let _ =
-  print_endline "nothing here yet"
+open Lwt
+open Lwt_io
+
+module Spotify = Spotify_interfaces.Org_mpris_MediaPlayer2_Player
+
+let run_command command proxy =
+  OBus_method.call command proxy ()
+
+let commands =
+  [
+    "pp", ("Play/pause", Spotify.m_PlayPause);
+    "p", ("Previous", Spotify.m_Previous);
+    "n", ("Next", Spotify.m_Next);
+  ]
+
+(* Print usage text. *)
+let usage () =
+  let usage_text = [
+    "Usage:";
+    Printf.sprintf "%s <command>" Sys.argv.(0);
+    "";
+    "Available commands:";
+  ]
+  @
+  (List.map
+    (fun (command, (doc, _)) -> Printf.sprintf "%s: %s" command doc)
+    commands)
+  in
+  List.iter print_endline usage_text
+
+lwt _ =
+  let command = begin
+    try
+      let command_id = Sys.argv.(1) in
+      snd (List.assoc command_id commands)
+    with _ ->
+      usage ();
+      exit 1
+  end in
+
+  lwt bus = OBus_bus.session () in
+  let proxy =
+    OBus_proxy.make
+      (OBus_peer.make bus "org.mpris.MediaPlayer2.spotify")
+      ["org"; "mpris"; "MediaPlayer2"]
+  in
+
+  try_lwt
+    run_command command proxy
+  with
+    | OBus_bus.Name_has_no_owner _
+    | OBus_bus.Service_unknown _ ->
+      lwt () = printl "Spotify service not found - is it running?" in
+      exit 1
+    | exn ->
+      raise_lwt exn
