c2cac33284daa08f8a4a238ae12f9f2f4bf02e7d
John Else <john.else@citrix.com>
2013-05-28 09:08:29 +0100

    Slight improvement to error handling
    Signed-off-by: John Else <john.else@citrix.com>

--- src/spotify_commands.ml
+++ src/spotify_commands.ml
@@ -3,6 +3,8 @@ open Lwt_io
 
 module Spotify = Spotify_client.Org_mpris_MediaPlayer2_Player
 
+exception No_results
+
 let with_proxy f =
   lwt bus = OBus_bus.session () in
   let proxy = Spotify_proxy.of_bus bus in
@@ -23,9 +25,11 @@ let next () =
 let play_album album_name =
   let play_album_lwt =
     lwt results = Spotify_search.search_albums album_name in
-    let album = List.nth results.Spotify_search_t.albums 0 in
-    with_proxy
-      (fun proxy -> Spotify.open_uri proxy album.Spotify_search_t.album_href)
+    match results.Spotify_search_t.albums with
+    | album :: _ ->
+      with_proxy
+        (fun proxy -> Spotify.open_uri proxy album.Spotify_search_t.album_href)
+    | [] -> Lwt.fail No_results
   in
   Lwt_main.run play_album_lwt
 
@@ -35,9 +39,11 @@ let play_pause () =
 let play_track track_name =
   let play_track_lwt =
     lwt results = Spotify_search.search_tracks track_name in
-    let track = List.nth results.Spotify_search_t.tracks 0 in
-    with_proxy
-      (fun proxy -> Spotify.open_uri proxy track.Spotify_search_t.track_href)
+    match results.Spotify_search_t.tracks with
+    | track :: _ ->
+      with_proxy
+        (fun proxy -> Spotify.open_uri proxy track.Spotify_search_t.track_href)
+    | [] -> Lwt.fail No_results
   in
   Lwt_main.run play_track_lwt
 
