ac891cca16fe190c3e0f441f7cb5d32c75f629f6
John Else <john.else@citrix.com>
2014-12-22 02:57:03 +0000

    travis: Update OCaml and OPAM versions
    Signed-off-by: John Else <john.else@citrix.com>

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -3,12 +3,10 @@ OPAM_PACKAGES="mpris spotify-web-api cmdliner"
 
 
 case "$OCAML_VERSION,$OPAM_VERSION" in
-3.12.1,1.0.0) ppa=avsm/ocaml312+opam10 ;;
 3.12.1,1.1.0) ppa=avsm/ocaml312+opam11 ;;
-4.00.1,1.0.0) ppa=avsm/ocaml40+opam10 ;;
 4.00.1,1.1.0) ppa=avsm/ocaml40+opam11 ;;
-4.01.0,1.0.0) ppa=avsm/ocaml41+opam10 ;;
 4.01.0,1.1.0) ppa=avsm/ocaml41+opam11 ;;
+4.02.1,1.1.0) ppa=avsm/ocaml42+opam11 ;;
 *) echo Unknown $OCAML_VERSION,$OPAM_VERSION; exit 1 ;;
 esac
 
